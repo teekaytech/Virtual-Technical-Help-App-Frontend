650fb5a650e5f76425e484527707661b776d5387
teekaytech <taofeek.olalere@feghas.com>
2020-12-02 22:39:13 +0100

    Separate components for authenticated user and other users

--- src/App.js
+++ src/App.js
@@ -6,39 +6,60 @@ import Navbar from './components/Navbar';
 import Dashboard from './containers/Dashboard';
 import Home from './containers/Home';
 import { checkLoginStatus } from './actions/auth';
+import { LOGGED_IN } from './actions/types';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
 
-const App = ({ checkLoginStatus }) => {
+const App = ({ checkLoginStatus, status, loading }) => {
   useEffect(() => {
     checkLoginStatus();
   }, [checkLoginStatus]);
 
+  if (loading) {
+    return (
+      <p>Loading...</p>
+    );
+  }
+
+  if (status === LOGGED_IN) {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header>
+            <Navbar />
+          </header>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/user/:user_id/dashboard" component={Dashboard} />
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
+    );
+  }
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header>
-          <Navbar />
-        </header>
-        <main>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/user/:user_id/dashboard" component={Dashboard} />
-          </Switch>
-        </main>
-      </div>
-    </BrowserRouter>
+    <div>
+      <Login />
+      <Signup />
+    </div>
   );
 };
 
 App.defaultProps = {
-  checkLoginStatus: PropTypes.func,
+  checkLoginStatus: PropTypes.fuloadingnc,
 };
 
 App.propTypes = {
   checkLoginStatus: PropTypes.func,
+  status: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
   status: state.auth.loggedIn,
+  loading: state.auth.loading,
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Login from '../components/auth/Login';
-import Signup from '../components/auth/Signup';
 
 function Home() {
   return (
     <section>
       <h1>Welcome Home</h1>
-      <Signup />
-      <Login />
     </section>
   );
 }
